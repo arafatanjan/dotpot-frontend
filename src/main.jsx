b096f3a5c536205a8aa95fb3aba50c4fa16a14d4
arafatanjan <arafatanjan@gmail.com>
2024-11-03 12:16:50 +0600

    Deploy commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,20 @@
+// import React from "react";
+// import { StrictMode } from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+// import { RouterProvider } from "react-router-dom";
+// import { router } from "./Routes/Routes.jsx";
+// import AuthProvider from "./AuthProvider/AuthProvider.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <AuthProvider>
+//     <RouterProvider router={router} />
+//     </AuthProvider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
@@ -5,12 +22,9 @@ import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
-import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
     <RouterProvider router={router} />
-    </AuthProvider>
   </React.StrictMode>
 );
